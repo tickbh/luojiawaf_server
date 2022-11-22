1c9d661b0d89cd069200c7ad1ad2bb537f7cfcb2
tickbh <tickdream125@hotmail.com>
2022-11-22 09:52:28 +0800

    init default sqlite

--- LuojiaWaf/settings.py
+++ LuojiaWaf/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     }
 }
 
+import os.path
+if not os.path.isfile(BASE_DIR / 'db.sqlite3'):
+    import shutil
+    shutil.copyfile(BASE_DIR / 'db_base.sqlite3', BASE_DIR / 'db.sqlite3')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
